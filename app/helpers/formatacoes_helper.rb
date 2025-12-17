c8df596e84efde976deae39f84ec12286fdd486c
castagnarodavid9-sketch <castagnarodavid9@gmail.com>
2025-12-17 10:53:24 -0300

    Arrumados campos para usufruir dos helpers criados. refs #W_171220251019

---
+++ app/helpers/formatacoes_helper.rb
@@ -0,0 +1,13 @@
+module FormatacoesHelper
+  def formata_datas(data)
+    return "" if !data.present?
+    date = data.strftime("%d/%m/%Y")
+    return date
+  end
+
+  def formata_moedas(valor)
+    return "" if !valor.present?
+    value = number_to_currency(valor, unit: "R$", separator: ",", delimiter: ".")
+    return value
+  end
+end
