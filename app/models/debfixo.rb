a95ffb6ce95ea2727a54d0f06cc5f74999ff5631
castagnarodavid9-sketch <castagnarodavid9@gmail.com>
2025-12-18 16:52:32 -0300

    Ajustado form de debfixos e colocadas validações de presença e regras de negócio para as datas. refs #181220251323

--- app/models/debfixo.rb
+++ app/models/debfixo.rb
@@ -3,6 +3,22 @@ class Debfixo < ApplicationRecord
   belongs_to :contrato
   scope :nao_quitado, -> { where(:quitado => false)}
   before_save :gera_competencia
+  validate :valida_campos
+
+  def valida_campos
+    # Valida campos presença
+    errors.add(:base, "Nome de débito fixo não foi informado!") if !self.nome_debfx.present?
+    errors.add(:base, "Valor de débito fixo não foi informado!") if !self.valor_debfx.present?
+
+    # Valida valores
+    errors.add(:base, "Nome informado não é válido!") if self.nome_debfx.match?(/[*_|\/]/)
+    errors.add(:base, "Formato de valor não aceito!") if self.valor_debfx.to_s.match?(/\A\d+\z/)
+    errors.add(:base, "Débito não pode ser menor que R$ 0, 00 reais!") if self.valor_debfx <= 0
+
+    # Valida regra de negocio
+    errors.add(:base, "Data inicial do débito não pode ser maior que a data final!") if CalculosSistema.gera_cmpt(self.cmpt_ini) > CalculosSistema.gera_cmpt(self.cmpt_fim)
+    errors.add(:base, "Data inicial do débito não pode ser igual a data final!") if CalculosSistema.gera_cmpt(self.cmpt_ini) == CalculosSistema.gera_cmpt(self.cmpt_fim)
+  end
 
   private
   def gera_competencia
